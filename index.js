14903788d8f84b51d1b08e70e143b41239b9d797
dpesic21 <dpesic@veleri.hr>
2025-06-03 21:10:35 +0200

    Dodan je provjera da li postoje neke razlike u prijašnjim podatcima

--- index.js
+++ index.js
@@ -2,10 +2,38 @@ const puppeteer = require('puppeteer');
 const express = require('express');
 const { Parser } = require('json2csv');  // Za konverziju u CSV
 const fs = require('fs');  // Za rad s datotekama
+const nodemailer = require('nodemailer');
+const { diff } = require('deep-diff');  // Za usporedbu podataka
 const app = express();
 const PORT = 8000;
 
 const url = 'https://www.cix.hr/en/members/members'; // URL stranice koju scrapiramo
+
+// Funkcija za slanje e-maila
+function sendEmailNotification(differences) {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'dariopesic21@gmail.com',  // Vaš email
+            pass: 'Gunner4life'              // Vaša email lozinka
+        }
+    });
+
+    const mailOptions = {
+        from: 'dariopesic21@gmail.com',  // Pošiljatelj
+        to: 'manexy777@gmail.com',       // Primatelj
+        subject: 'Razlika u CSV podacima', // Predmet emaila
+        text: `Otkrivene su razlike u CSV podacima:\n\n${JSON.stringify(differences, null, 2)}`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log('Greška pri slanju emaila:', error);
+        } else {
+            console.log('Email poslan: ' + info.response);
+        }
+    });
+}
 
 app.get('/scrape', async (req, res) => {
     console.log("Ruta '/scrape' je pozvana");
@@ -19,7 +47,7 @@ app.get('/scrape', async (req, res) => {
         // Čekanje dok tablica postane dostupna
         await page.waitForSelector('table');
 
-        // Dohvati HTML sadržaj stranice za logiranje
+        // Dohvati HTML sadržaj stranice
         const html = await page.content();
         console.log('HTML sadržaj stranice:', html);
 
@@ -62,6 +90,25 @@ app.get('/scrape', async (req, res) => {
         const csvData = json2csvParser.parse(membersData);
         console.log('Spremanje CSV datoteke...');
         fs.writeFileSync('members_data.csv', csvData);  // Spremanje CSV datoteke
+
+        // Učitavanje prethodnih podataka za usporedbu
+        let previousData = [];
+        if (fs.existsSync('previous_members_data.json')) {
+            previousData = JSON.parse(fs.readFileSync('previous_members_data.json', 'utf8'));
+        }
+
+        // Usporedba podataka
+        const differences = diff(previousData, membersData);
+
+        if (differences) {
+            console.log('Razlike između novih i prethodnih podataka:', differences);
+            sendEmailNotification(differences);  // Pozivanje funkcije za slanje emaila
+        } else {
+            console.log('Nema razlika u podacima.');
+        }
+
+        // Spremanje novih podataka kao prethodnih podataka
+        fs.writeFileSync('previous_members_data.json', JSON.stringify(membersData, null, 4));
 
         // Obavještavamo korisnika da su podaci spremljeni
         res.send('Podaci su uspješno spremljeni u JSON i CSV datoteku.');
